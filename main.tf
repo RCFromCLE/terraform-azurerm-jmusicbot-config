8bda50a4ab58ac3547946d9e0c03192d8acaaba2
Rudy <rudycorradetti4@gmail.com>
2024-07-18 10:19:08 -0400

    updated public key to be openssh format 1.2.6

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ provider "azurerm" {
 #################################### module block ##################################
 module "jmusicbot" {
   source  = "RCFromCLE/jmusicbot/azure"
-  version = "1.2.6"
+  version = "1.2.7"
 
   # required variables
   azure_tenant_id     = var.azure_tenant_id
