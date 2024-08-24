8c581f999bfe056ed5e50edd04ee3a3ead984676
Rudy Corradetti <79128640+RCFromCLE@users.noreply.github.com>
2024-08-24 04:43:23 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ provider "azurerm" {
 #################################### module block ###################################
 module "jmusicbot" {
   source  = "RCFromCLE/jmusicbot/azure"
-  version = "v2.0.4"
+  version = "v2.0.5"
 
   # required variables
   azure_tenant_id     = var.azure_tenant_id
